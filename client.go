4eef3f9c235984b86b47986c4199c6b0939a1476
Andrew Stanishevskyi <37955212+astanishevskyi@users.noreply.github.com>
2021-05-20 12:30:43 +0300

    Update client.go
    Co-authored-by: Dmytro Brykovets <dmytro.brykovets@gmail.com>

--- client.go
+++ client.go
@@ -14,15 +14,31 @@ func main() {
 		return
 	}
 
+	// Readers and writers are created here only once.
+	inputReader := bufio.NewReader(os.Stdin)
+	connReader := bufio.NewReader(conn)
+	connWriter := bufio.NewWriter(conn)
+	
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		conn.Write([]byte(text + "\n"))
-		message, err := bufio.NewReader(conn).ReadString('\n')
+		input, err := inputReader.ReadBytes('\n') // ReadBytes eliminates the need of conversion to bytes. 
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Print("->" + message)
+		
+		// Using a buffered writer here is optional. Just for example.
+		connWriter.Write(input)
+		connWriter.WriteByte('\n')
+		if err := connWriter.Flush(); err != nil {
+			fmt.Println(err)
+			return
+		}
+		
+		message, err := connReader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Print("->", message) // The same result, but without string concatination.
 	}
 }
